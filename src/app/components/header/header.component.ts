1079124be395b5babb8efcabea7f2074fbbdf016
luanamcomin <luana.martinscomin@gmail.com>
2024-05-25 23:03:13 -0300

    Ajustes no navbar

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,14 +1,17 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { materialize } from 'rxjs';
 
+
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [
+    MatToolbarModule,
+    NgOptimizedImage],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
 }
