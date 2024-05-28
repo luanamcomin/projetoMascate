adf1ba77deb6c9ca8fd51b6e87c385c579b8de30
luanamcomin <luana.martinscomin@gmail.com>
2024-05-27 22:00:00 -0300

    Configurando rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component'
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OrderCatalogComponent } from './catalogo/order-catalog/order-catalog.component';
+import { ModuleWithProviders } from '@angular/core';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'catalogo', component: OrderCatalogComponent },
+  { path: 'home', component: HomeComponent },
+  {path: '', component: AppComponent},
+];
+
+const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(routes);
+
+export { routing };
