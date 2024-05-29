194b7ebb55db63e13cbc0db9be5786d568593444
luanamcomin <luana.martinscomin@gmail.com>
2024-05-29 07:33:47 -0300

    Ajustes de erros de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { OrderCatalogComponent } from './catalogo/order-catalog/order-catalog.component';
 import { ModuleWithProviders } from '@angular/core';
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: 'catalogo', component: OrderCatalogComponent },
   { path: 'home', component: HomeComponent },
-  {path: '', component: AppComponent},
+  { path: '', component: LoginComponent }
 ];
 
 const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(routes);
