f0bf5f3eb7effb52123a6b244ddefd17dad10ac0
luanamcomin <luana.martinscomin@gmail.com>
2024-06-01 14:30:57 -0300

    Ajustes de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,16 @@ import { HomeComponent } from './home/home.component';
 import { OrderCatalogComponent } from './catalogo/order-catalog/order-catalog.component';
 import { ModuleWithProviders } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { GestaoComponent } from './gestao/gestao.component';
+import { HistoricoComponent } from './historico/historico.component';
+import { PedidoComponent } from './pedido/pedido.component';
+import { RelatoriosComponent } from './relatorios/relatorios.component';
 
 export const routes: Routes = [
+  { path: 'relatorios', component: RelatoriosComponent},
+  { path: 'historico', component: HistoricoComponent},
+  { path: 'pedido', component: PedidoComponent},
+  { path: 'gestao', component: GestaoComponent},
   { path: 'catalogo', component: OrderCatalogComponent },
   { path: 'home', component: HomeComponent },
   { path: '', component: LoginComponent }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { OrderCatalogComponent } from '../catalogo/order-catalog/order-catalog.component';
+import { GestaoComponent } from '../gestao/gestao.component';
+import { HistoricoComponent } from '../historico/historico.component';
+import { PedidoComponent } from '../pedido/pedido.component';
+import { RelatoriosComponent } from '../relatorios/relatorios.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, OrderCatalogComponent,
+            GestaoComponent, HistoricoComponent, PedidoComponent, RelatoriosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
