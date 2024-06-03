beaa96d2795b55877b56b71cb4486fad2c936c35
luanamcomin <luana.martinscomin@gmail.com>
2024-06-02 21:31:08 -0300

    Adicionando o componente 'tabela'

---
+++ src/app/components/tabelas/tabelas.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+export interface PeriodicElement {
+  pedido: number;
+  data: number;
+  unidade: string;
+  status: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {pedido: 123456, data: 11, unidade: 'Lanchonete', status: 'Em processamento'},
+  {pedido: 654321, data: 13, unidade: 'Cafeteria', status: 'Confirmado'},
+  {pedido: 645342, data: 14, unidade: 'Lanchonete', status: 'Finalizado'},
+  {pedido: 132435, data: 15, unidade: 'Cafeteria', status: 'Finalizado'},
+  {pedido: 321456, data: 16, unidade: 'Cafeteria', status: 'Finalizado'},
+  {pedido: 654123, data: 20, unidade: 'Lanchonete', status: 'Cancelado'},
+  {pedido: 546312, data: 21, unidade: 'Lanchonete', status: 'Finalizado'}
+];
+
+/**
+ * @title Table with filtering
+ */
+@Component({
+  selector: 'app-tabelas',
+  styleUrl: './tabelas.component.css',
+  templateUrl: './tabelas.component.html',
+  standalone: true,
+  imports: [MatFormFieldModule, MatInputModule, MatTableModule],
+})
+export class TabelasComponent {
+  displayedColumns: string[] = ['pedido', 'data', 'unidade', 'status'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
+
