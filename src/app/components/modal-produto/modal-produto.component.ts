f4d5e2f9e6a440cf8849d2edc54c4535c9b6bea3
luanamcomin <luana.martinscomin@gmail.com>
2024-06-05 18:03:12 -0300

    Criando o componente Modal

---
+++ src/app/components/modal-produto/modal-produto.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-modal-produto',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule],
+  templateUrl: './modal-produto.component.html',
+  styleUrl: './modal-produto.component.css'
+})
+export class ModalProdutoComponent {
+}
