9e202efd384ceb5081d36d8f9aedb157c3c84d37
luanamcomin <luana.martinscomin@gmail.com>
2024-06-05 18:09:19 -0300

    .

--- src/app/catalogo/order-catalog.component.ts
+++ src/app/catalogo/order-catalog.component.ts
@@ -1,18 +1,41 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
-import { RouterOutlet } from '@angular/router';
+import { ModalProdutoComponent } from '../components/modal-produto/modal-produto.component';
+import { ProductCardComponent } from '../components/product-card/product-card.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-order-catalog',
   standalone: true,
   imports: [
+    ModalProdutoComponent,
     HeaderComponent,
     FooterComponent,
-    RouterOutlet],
+    RouterOutlet,
+    ProductCardComponent,
+    MatExpansionModule,
+    MatButtonModule,
+    MatDialogTitle, MatDialogContent, MatDialogActions, MatDialogClose, MatButtonModule
+  ],
   templateUrl: './order-catalog.component.html',
   styleUrl: './order-catalog.component.css'
 })
-export class OrderCatalogComponent {
 
+export class OrderCatalogComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(ModalProdutoComponent);
+  }
 }
+
