415047ccdc20930e0f1f9548df53ee37c6bafd96
luanamcomin <luana.martinscomin@gmail.com>
2024-06-19 00:23:05 -0300

    Adicionando códigos feitos por Amanda

--- src/app/pages/gestao/gestao.component.ts
+++ src/app/pages/gestao/gestao.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { GestaoService } from '../../services/gestao/gestao.service';
 
 @Component({
   selector: 'app-gestao',
@@ -9,10 +13,22 @@ import { FooterComponent } from '../../components/footer/footer.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    FooterComponent],
+    FooterComponent,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatExpansionModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './gestao.component.html',
   styleUrl: './gestao.component.css'
 })
 export class GestaoComponent {
+  readonly panelOpenState = signal(false);
 
+  constructor(private gestaoService: GestaoService) { }
+
+  finalizarPedido(numeroPedido: number) {
+    // Lógica para finalizar o pedido com o número recebido
+    console.log(`Pedido ${numeroPedido} finalizado.`);
+    // Aqui você pode implementar lógica adicional, como chamar serviços, atualizar estados, etc.
+  }
 }
--- src/app/pages/historico/historico.component.ts
+++ src/app/pages/historico/historico.component.ts
@@ -1,17 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { NgForOf, CommonModule, JsonPipe } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
-  selector: 'app-historico',
+  selector: 'pages-historico',
   standalone: true,
-  imports: [RouterOutlet,
-            HeaderComponent,
-            FooterComponent],
+  providers: [provideNativeDateAdapter()],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgForOf, CommonModule, MatFormFieldModule, MatDatepickerModule, MatSelectModule],
   templateUrl: './historico.component.html',
-  styleUrl: './historico.component.css'
+  styleUrls: ['./historico.component.css']
 })
-export class HistoricoComponent {
+export class HistoricoComponent implements OnInit {
+  pedidos: { id: number, data: Date, hora: string, unidade: string, status: string }[] = [
+    { id: 1, data: new Date('2024-05-11'), hora: '10:30', unidade: 'Lanchonete', status: 'Concluído' },
+    { id: 2, data: new Date('2024-05-13'), hora: '15:00', unidade: 'Cafeteria', status: 'Concluído' },
+    { id: 3, data: new Date('2024-05-14'), hora: '14:45', unidade: 'Lanchonete', status: 'Concluído' },
+    { id: 4, data: new Date('2024-05-15'), hora: '15:00', unidade: 'Cafeteria', status: 'Concluído' },
+    { id: 5, data: new Date('2024-05-16'), hora: '08:00', unidade: 'Cafeteria', status: 'Concluído' },
+    { id: 6, data: new Date('2024-05-20'), hora: '09:15', unidade: 'Lanchonete', status: 'Concluído' },
+    { id: 7, data: new Date('2024-05-21'), hora: '11:00', unidade: 'Lanchonete', status: 'Concluído' },
+  ];
 
+  filtro: string | null = null;
+  filtroPropriedade: string = 'id';
+  ordem: string = 'mais-novos';
+  ordenacaoAscendente: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.ordenar(); // Ordenar ao iniciar
+  }
+
+  filtrar(event: Event): void {
+    const valor = (event.target as HTMLInputElement).value;
+    this.filtro = valor !== '' ? valor : null;
+  }
+
+  filtrarPropriedade(event: Event): void {
+    const propriedade = (event.target as HTMLSelectElement).value;
+    this.filtroPropriedade = propriedade;
+  }
+
+  alternarOrdenacao(): void {
+    this.ordenacaoAscendente = !this.ordenacaoAscendente;
+    this.ordenar();
+  }
+  ordenar(): void {
+    this.pedidos.sort((a, b) => {
+      const dataA = new Date(a.data).getTime();
+      const dataB = new Date(b.data).getTime();
+      return this.ordenacaoAscendente ? dataA - dataB : dataB - dataA;
+    });
+  }
+  get filteredPedidos(): any[] {
+    let pedidosFiltrados = this.pedidos;
+
+    if (this.filtro) {
+      pedidosFiltrados = pedidosFiltrados.filter((pedido) => {
+        const propriedade = this.filtroPropriedade as keyof typeof pedido;
+        const valorPropriedade = pedido[propriedade];
+        return valorPropriedade ? valorPropriedade.toString().toLowerCase().includes(this.filtro!.toLowerCase()) : false;
+      });
+    }
+
+    return pedidosFiltrados;
+  }
 }
+
+
